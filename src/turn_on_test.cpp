2b8cc81f5c393e7903c800795b4de92d90729f2d
pastorofmuppetz <pankiewiczkuba@gmail.com>
2016-12-11 16:11:14 +0100

    Włączanie morfo i zapis do pliku - v. 0.1

--- src/turn_on_test.cpp
+++ src/turn_on_test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(void){
 	system("chcp 1250");
-	system("java -jar morfologik-tools-2.1.0/lib/morfologik-tools-2.1.0.jar dict_apply -d polish.dict > out.txt");
+	system("java -jar morfologik-tools-2.1.0.jar dict_apply -d polish.dict > out.txt");
 	//@up ścieżka, przy założeniu, że morfologik jest w tym samym katalogu co plik odpalający
 	return 0;
 }
